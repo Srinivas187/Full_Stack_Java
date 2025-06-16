7b2838bb914c00f3468aacba9d4dd41bb748586f
Srinivas187 <srinivasmuragondi34@gmail.com>
2025-06-16 03:41:06 -0700

    Add files via upload

---
+++ operators/ArthematicOperator.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class ArthematicOperator
+{
+public static void main(String[] args)
+{
+float a=1;
+float b=2;
+System.out.println("Addition :"+(a+b));
+System.out.println("Substraction :"+(a-b));
+System.out.println("Division :"+(a/b));
+System.out.println("MUltiplication :"+(a*b));
+System.out.println("Modulus :"+(a%b));
+}
+}
